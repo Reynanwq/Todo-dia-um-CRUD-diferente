76dad60e603253613088b177f55e64793cfc076b
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-01-16 18:59:21 -0300

    atualizando index.js

--- 1° API/index.js
+++ 1° API/index.js
@@ -1,8 +1,11 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Person = require('./models/Person');
+const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+
 
 mongoose.set('strictQuery', false);
 
@@ -16,15 +19,21 @@ app.use( //PESQUISAR O QUE É ISSO
 
 /* --------- ROTAS API -------------- */
 
-/*
 const personRouter = require('./routes/personRoutes');
-app.use('/person', personRouter);*/
+app.use('/person', personRouter);
+
+
+mongoose.connect(`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@firtsapi.z0iev5l.mongodb.net/?retryWrites=true&w=majority
+`)
+
+
 
 /*  END POINT */
 
 app.get('/home', (req, res) => {
-    res.json({ message: 'Helo express' })
+    res.json({ message: 'Helo express, I am Reynan ' })
 });
 
 const port = 3000;
-app.listen(port, () => console.log(`Thef Server is runnig in the port ${port}`));
+app.listen(port, () => console.log(`The Server is runnig in the port ${port}`));
+//utilizar uma porta valida
