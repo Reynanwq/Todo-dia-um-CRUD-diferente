c5bc0ebf2cd09c9a7df6898ed5a115b76a9e1cb6
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-01-18 00:26:38 -0300

    add comentário

---
+++ 2° API/routes/personRouter.js
@@ -0,0 +1,14 @@
+/* INCIO MINHA API */
+const Router = require(express).Router();
+const Person = require();
+/* CRIAR USER */
+
+/* LER DADOS E MANDAR PARA DATABAS */
+
+/* ATUAZIZAR USUARIO PELO ID */
+
+/* DELETAR USUARIO PELO ID */
+
+/* DELETAR TODOS OS USUARIOS */
+
+/* FINAL MINHA API */
