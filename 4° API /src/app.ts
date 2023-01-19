a474d3960803adc87a4b7048b066834aaf61c064
Reynanwq <reynanwq@gmail.com>
2023-01-19 20:49:03 -0300

    4° api executando com sucesso

--- 4° API /src/app.ts
+++ 4° API /src/app.ts
@@ -1,7 +1,7 @@
 import * as express from 'express'; //importando o express com tudo que está lá dentro
 import * as cors from 'cors'; //padrao dew importação typescript
 import * as logger from 'morgan';
-
+import { router } from './routes/index';
 
 //exportar o meu app
 export const app = express();
@@ -15,3 +15,8 @@ app.use(logger('dev')); //No loguer aparec todas as requisições que foram util
 /* ------------- com essas aplicações, eu estou abrindo todas as portas -------- */
 
 //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s
+
+
+//INTEGRA O ENDPOINT NA APLICACÇÃO
+
+app.use('/', router)
