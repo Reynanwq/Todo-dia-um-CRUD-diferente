d40fb3778bfdca4c11e2040d0ea6695c090af97a
Reynanwq <reynanwq@gmail.com>
2023-01-19 05:34:05 -0300

    trabalhando no app.ts

---
+++ 4° API /src/app.ts
@@ -0,0 +1,7 @@
+import * as express from 'express'; //importando o express com tudo que está lá dentro
+import * as cors from 'cors';
+import * as logger from 'morgan';
+
+
+//exportar o meu app
+export con
