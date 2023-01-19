c824d8a658eac1327b5ed384a3236de5c3ae25a5
Reynanwq <reynanwq@gmail.com>
2023-01-19 19:21:54 -0300

    config do server realizadas com sucesso

--- 4° API /src/app.ts
+++ 4° API /src/app.ts
@@ -1,7 +1,17 @@
 import * as express from 'express'; //importando o express com tudo que está lá dentro
-import * as cors from 'cors';
+import * as cors from 'cors'; //padrao dew importação typescript
 import * as logger from 'morgan';
 
 
 //exportar o meu app
-export con
+export const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(logger('dev')); //No loguer aparec todas as requisições que foram utilizadas na minha API
+
+/* ------------- com essas aplicações, eu estou abrindo todas as portas -------- */
+
+//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s
--- 4° API /src/server.ts
+++ 4° API /src/server.ts
@@ -1,1 +1,16 @@
-//RESPONSAVEL PÓR STARTAR O SERVIDOR
+/* ------------------- RESPONSAVEL PÓR STARTAR O SERVIDOR ---------------*/
+
+/* ------------------- CONFIGURAÇÃO DO SERVIDOR O SERVIDOR ---------------*/
+
+import { app } from './app';
+
+const port = process.env.PORT || 3000; //vai pegar a porta que eu botei no .env ou 3000, já evita aquele tipo de erro
+
+const server = app.listen(port, () => console.log(`APP ouvindo na porta ${port}`)); //saber que a aplicação subiu
+
+//INTERRUPÇÃO EXPLICITA DO APP, PARA FECHAR PARA VALER
+
+process.on('SIGINT', () => {
+    server.close();
+    console.log('Aaplicação foi interrompida!');
+});
