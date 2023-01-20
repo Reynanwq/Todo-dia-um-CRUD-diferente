475f3ce6fdab0ce04bd033e7556f92bad1bd9fc9
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-01-20 14:57:56 -0300

    Método Post

--- 6° API/src/routes/index.ts
+++ 6° API/src/routes/index.ts
@@ -4,3 +4,48 @@ const router = Router();
 router.get('/', (req, res) => {
     res.status(200).send('Hello typescript')
 });
+
+//Criar usuário
+
+router.post("/", async(req, res) => {
+     const { name, age, email} = req.body;
+      if(!name){
+       res.status(442).json ({ message: 'O nome de usuário não existe'})
+      }
+       Const person = {
+       name,
+       age
+       email,
+     }
+      try{
+       await Person.create(person);
+       res.status(200).json({ message: 'usuário criado com sucesso'})
+      }catch(error){
+      res.status(500).Json({ error: error })
+     }
+});
+
+router.get("/", async(req, res) => {
+     try {
+      
+     }catch(error){
+      res.status(500).Json({ error: error })
+     }
+});
+
+
+router.patch("/", async(req, res) => {
+     try {
+      
+     }catch(error){
+      res.status(500).Json({ error: error })
+     }
+});
+
+router.delete("/", async(req, res) => {
+     try {
+      
+     }catch(error){
+      res.status(500).Json({ error: error })
+     }
+});
