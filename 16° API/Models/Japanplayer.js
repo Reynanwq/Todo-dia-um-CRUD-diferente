ecfa7eadc0482671ec4d77ba3756028ff56c6ea9
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-01-30 16:40:53 -0300

    request with database

---
+++ 16° API/Models/Japanplayer.js
@@ -0,0 +1,13 @@
+//arquivo importante para que a requisição com o banco de dados seja realizada
+const mongoose = require('mongoose');
+
+const Japanplayer = ('/ljl', {
+  id: Number,
+  nickName: String,
+  lane: String,
+  nationality: String,
+  team: String,
+  age: String
+});
+
+modules.exports = Japanplayer;
