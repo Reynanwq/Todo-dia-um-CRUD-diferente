51ccbefadde8c07ea8086099daf2c8ed9972b19d
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-01-30 19:48:52 -0300

    [add] main program

---
+++ 11° API/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();;
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const DB_USER = process.env.DB_USERNAME;
+const DB_PASSWORD  = process.env.DB_PASSWORD;
+
+app.use(express.json());
+
+app.use(
+  express.urlencoded({
+        extended: true,
+    }),
+);
+
+mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@apirest.5yq1hjx.mongodb.net/?retryWrites=true&w=majority`);
+.then(() => consolee.log('conexao com o MongoDb realizada com sucesso'))
+.catch((err) => console.log(err));
+
+const catRoutes = require('./Models/Cat');
+app.use('/', catRoutes);
+
+app.get('/home', (req, res) => {
+  res.status(200).json({ message: 'Hi litle Cat'})
+});
+
+const port = 3000;
+app.listen(port, () => console.log('Server Cat online'));
