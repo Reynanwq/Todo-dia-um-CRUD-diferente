e1097fde11872dd928a6856427c85fdc3c64e931
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-01-30 01:25:09 -0300

    principal prog add

---
+++ 9° API/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config(); //reuqisição do dotenv
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Person = require('./Models/Person');
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = encodeURIComponent(precess.env.DB_PASSWORD);
+
+app.use(express.json()); //forma para facilitar o uso do json
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+// ROTAS DA API
+const personRoutes = require('./routes/personRoutes'); 
+app.use('/', personRoutes);
+
+//ROTA INICIAL
+app.get('/home', (req, res) => {
+  res.status(200).json({ message: 'Hi 9° api'})
+});
+
+//fazendo conexao no mongoDB
+mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@apirest.5yq1hjx.mongodb.net/?retryWrites=true&w=majority`);
+.then(() => {
+  console.log('Conectando com o Banco de Dados');
+})
+.catch((err) => console.log(err));
+
+const port = 3000;
+app.listan(port, () => console.log('Server Online'));
