91e0630997020c8f450643a91b163ea880c066a6
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-01-30 19:22:23 -0300

    finish main program

---
+++ 10° API/index.js
@@ -0,0 +1,36 @@
+//requisitções
+require('dotenv').config();
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const MONGO_USERNAME = process.env.DB_USERNAME;
+const MONGO_PASSWORD = process.env.DB_PASSWORD;
+
+//facilitar leitura de um arquivo json
+app.use(express.json());
+
+//enlercoded
+
+app.use(
+  express.urlencoded({
+        extended: true,
+    }),
+)
+
+//banco de dados
+mongoose.connect(`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@apirest.5yq1hjx.mongodb.net/?retryWrites=true&w=majority`);
+.then(() => console.log('Conectamos ao banco de dados MongoDB'))
+.catch((err) => console.log(err));
+
+//enviar uma mensagem
+app.get('/home', (req, res) => {
+  res.status(200).json({ message: 'Hello Dog' })
+});
+
+//rota api
+const dogRoutes = require('./Models/Dog');
+app.use('/', dogRoutes);
+
+//ler
+const port = 3000;
+app.listen(port, () => console.log('Server Dog online'));
