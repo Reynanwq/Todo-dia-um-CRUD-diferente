47d1595442e86c96d882a15bd2c722c04b5f2f3a
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-01-31 02:07:05 -0300

    main program finish

---
+++ 12° API/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const DB_USER = process.env.DB_USERNAME; 
+const DB_PASSWORD = process.env.DB_PASSSWORD; 
+
+app.use(express.json());
+
+app.use(
+  express.unlencoded({
+    extended: true,
+  }),
+);
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apirest.5yq1hjx.mongodb.net/?retryWrites=true&w=majority`);
+.then(() => console.log('Conexao realizada com sucesso'))
+.catch((err) => console.log(err));
+
+const regioesRouter = require('./Models/Reginal');
+app.use('/', regioesRouter);
+
+app.get('/home', (req, res) => {
+   res.status(200).json({ message: 'Hi regions by league of legends'})
+});
+
+const port = 3000;
+app.listen(port, () => console.log('server online'));
