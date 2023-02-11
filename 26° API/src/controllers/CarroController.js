61c9d89f13fb610fa6a28d9e5729f9bbdbd99667
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-02-11 17:34:18 -0300

    [add] function buscarUm

--- 26° API/src/controllers/CarroController.js
+++ 26° API/src/controllers/CarroController.js
@@ -1,6 +1,7 @@
 const CarroService = require('../services/CarroService');
 
 modules.exports = {
+
 buscarTodos: async(req, res) => {
   let json = {error: '', result:[]};
   let carros = await CarroService.buscarTodos();
@@ -9,6 +10,16 @@ buscarTodos: async(req, res) => {
         codigo: carros[i].codigo,
         descricao: carros[i].modelo
       });
+    }
+    res.json(json);
+  },
+  
+  buscarUm: async(req, res) => {
+    let json = {error:'', result:{}};
+    let codigo = req.params.codigo;
+    let carro = await CarroService.buscarUm(codigo);
+    if(carro){
+      json.result = carro;
     }
     res.json(json);
   }
