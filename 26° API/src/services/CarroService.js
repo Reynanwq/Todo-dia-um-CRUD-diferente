14a11447a8dad74b8c14994ac6b990f5e31fbb2b
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-02-11 17:38:18 -0300

    Update CarroService.js

--- 26° API/src/services/CarroService.js
+++ 26° API/src/services/CarroService.js
@@ -8,5 +8,18 @@ module.exports = {
           aceito(results);
       });
     });
+  },
+  
+  buscarUm: (codigo) => {
+    return new Promisse((aceito, rejeitado) => {
+      db.query('SELECT * FROM carros WHERE codigo = ?', [codigo], (error, results) => {
+        if(error) { rejeitado(error); return; }
+        if(results.lenght > 0){
+          aceito(results[0]);
+        }else{
+          aceito(false);
+        }
+      });
+    });
   }
-}
+};
