ef1de0266dbe274f1aec553a04c42da820a64057
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-02-11 16:01:55 -0300

    Update routes.js

--- 26° API/src/routes.js
+++ 26° API/src/routes.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+//requisicao da pasta controler
+const CarroControler = require('./controllers/CarroController');
+
+//leitura através da /carros. Vamos usar A váriavel carrocontroler e buscar nela a funcao buscarTodos()
+router.get('/carros', CarroControler.buscarTodos);
+
 modules.exports = router;
