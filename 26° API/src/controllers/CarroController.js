025029ef39b53ce0bc6cf6e1620d8602e8749e83
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-02-11 16:27:15 -0300

    Update CarroController.js

--- 26° API/src/controllers/CarroController.js
+++ 26° API/src/controllers/CarroController.js
@@ -1,1 +1,17 @@
 const CarroService = require('../services/CarroService');
+
+modules.exports = {
+buscarTodos: async(req, res) => {
+  let json = {error: '', result:[]};
+  let carros = await CarroService.buscarTodos();
+    for(let i in carros){
+      json.result.push({
+        codigo: carros[i].codigo,
+        descricao: carros[i].modelo
+      });
+    }
+    res.json(json);
+  }
+}
+
+
