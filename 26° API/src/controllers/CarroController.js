113fe6d7b08938e89cf2e25e99d4847834d336e9
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-02-12 00:57:36 -0300

    Update CarroController.js

--- 26° API/src/controllers/CarroController.js
+++ 26° API/src/controllers/CarroController.js
@@ -22,7 +22,54 @@ buscarTodos: async(req, res) => {
       json.result = carro;
     }
     res.json(json);
-  }
+  },
+  
+  inserir: async(req, res) => {
+    let json = {error:'', result:{}};
+    let modelo = req.body.modelo;
+    let placa = req.body.placa;
+   
+    if(modelo && placa){
+      let CarroCodigo = await CarroService.inserir(modelo, placa);
+      json.result = {
+          codigo: CarroCodigo,
+          modelo,
+          placa
+       };
+    }else{
+      json.error = 'Campos não enviados';
+    }
+    res.json(json);
+  },
+  
+   alterar: async(req, res) => {
+        let json = {error:'', result:{}};
+
+        let codigo = req.params.codigo;
+        let modelo = req.body.modelo;
+        let placa = req.body.placa;
+
+        if (codigo && modelo && placa){
+            await CarroService.alterar(codigo, modelo, placa);
+            json.result = {
+                codigo,
+                modelo,
+                placa
+            };
+        }else{
+            json.error = 'Campos não enviados';
+        }
+        res.json(json);
+    },
+    
+    excluir: async(req, res) => {
+        let json = {error:'', result:{}};
+
+        await CarroService.excluir(req.params.codigo);
+        
+        res.json(json);
+    },
+  
 }
 
 
