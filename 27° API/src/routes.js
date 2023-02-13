e7acb5f8a8519af630263d597749dd83cb737905
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-02-13 17:34:32 -0300

    Update routes.js

--- 27° API/src/routes.js
+++ 27° API/src/routes.js
@@ -5,5 +5,8 @@ const carroController = require('./controllers/carroController');
 
 router.get('/carros', carroController.buscarTodos);
 route.get('/carro/:codigo', carroController.buscarUm);
+router.post('/carro', carroController.inserir);
+router.patch('/carro/:codigo', carroController.update);
+router.delete('/carro/:codigo', carroController.delete);
 
 modules.exports = router;
