b12cb975315e7a9d19fb38890656bfc5e82c8ac0
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-02-20 12:52:19 -0300

    Create eslint.js

---
+++ 30° API/eslint.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    "no-console": "off"
+  },
+};
